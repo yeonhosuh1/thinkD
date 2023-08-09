6e82b474b3107d7944c626a9a286e76e6f47ce35
Yeonho Suh <yeonhosuh1@naver.com>
2023-08-09 23:19:01 +0900

    home->map button add

--- app/src/main/java/com/example/thinkd/HomeActivity.java
+++ app/src/main/java/com/example/thinkd/HomeActivity.java
@@ -2,14 +2,11 @@ package com.example.thinkd;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.thinkd.menu.CafeKioskActivity;
-
-import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,6 +17,12 @@ public class HomeActivity extends AppCompatActivity {
 
         Button bt_map = findViewById(R.id.buttonMap);
         bt_map.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+            startActivity(intent);
+        });
+
+        Button bt_kiosk = findViewById(R.id.bt_kiosk);
+        bt_kiosk.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), CafeKioskActivity.class);
             startActivity(intent);
         });
