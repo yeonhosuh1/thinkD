cf46d2b6623989a85cde552b2d30666ff4292151
Yeonho Suh <yeonhosuh1@naver.com>
2023-08-16 19:38:54 +0900

    MenuRecommend init

---
+++ app/src/main/java/com/example/thinkd/recommend/MenuRecommend.java
@@ -0,0 +1,5 @@
+package com.example.thinkd.recommend;
+
+public class MenuRecommend {
+    //TODO : DB에서 가장 선택 빈도가 높은 메뉴 받아오기
+}
---
+++ app/src/main/java/com/example/thinkd/recommend/TestDataCreate.java
@@ -0,0 +1,5 @@
+package com.example.thinkd.recommend;
+
+public class TestDataCreate {
+    // TODO: 테스트 데이터 생성
+}
